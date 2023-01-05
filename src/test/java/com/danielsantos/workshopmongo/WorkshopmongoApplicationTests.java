b061ee549b4ef2e6709399fd380b6c643b2f79d8
Daniel Santos <leinadss2011@gmail.com>
2023-01-05 17:53:32 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/test/java/com/danielsantos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsantos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
