8fffd8048464e18dccda584ca996d59c6aaf1fcf
Daniel Santos <leinadss2011@gmail.com>
2023-01-10 12:07:13 -0300

    Obtendo um usuario por id

--- src/main/java/com/danielsantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danielsantos/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,10 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {	
+		User obj = service .findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 	}
 
--- src/main/java/com/danielsantos/workshopmongo/services/UserService.java
+++ src/main/java/com/danielsantos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.danielsantos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danielsantos.workshopmongo.domain.User;
 import com.danielsantos.workshopmongo.repository.UserRepository;
+import com.danielsantos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService{
@@ -17,6 +19,11 @@ public class UserService{
 	
 	 public List<User> findAll(){
 		 return repo.findAll();
+	 }
+	 
+	 public User findById(String id) {
+		 Optional<User> obj = repo.findById(id);
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	 }
 	
 }
